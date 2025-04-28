092ece87a84a1e50e786d910e785e7d0ce086c90
arunima <arunimasinghhjp12@gmail.com>
2025-04-28 22:20:23 +0530

    last onehr

---
+++ 03_basics/04_control.js
@@ -0,0 +1,25 @@
+//if
+
+if (true) {
+    console.log();
+}
+
+// < , >, <= , >=, ==, != comparison operator
+//var => completely global scope
+
+//falsy values
+// false, 0, -0, BigInt0n,"", null, undefined, NaN
+
+//truthy value
+// "0", 'false', " ", [], {}, function(){}
+
+
+// nullish coalescing operator (??) : null undefined
+
+let val1
+val1=null ?? 10;
+console.log(val1) //returns 10
+
+//ternary operator 
+
+// condition ? true : false 
---
+++ 04_loop/one.js
@@ -0,0 +1,34 @@
+//for
+
+const array=[1,2,3,4,6]
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if(element==5){
+        console.log("5 is the best number");
+        
+    }
+    console.log(element);
+    
+}
+
+
+
+// while (condition) {
+    
+// }
+
+// do {
+//     console.log("arunima")
+// } while (true);
+
+//for of loop
+const arr1=[1,2,3,4,5,52]
+for (const i of arr1) {
+    console.log(i);
+    
+}
+
+const myObject={
+    js: "javascript",
+    
+}
